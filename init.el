5feec4bdfee91b374bbbbb3349b740e0d3835541
Rafael Villarroel <rvf0068@gmail.com>
2017-11-02 07:58:13 -0600

    First version that seems to work
    - using use-package
    - configuration in an org-mode file
    - installing in the directory where init.el is found

--- init.el
+++ init.el
@@ -1,25 +1,41 @@
-(defconst scimax-dir (file-name-directory (or load-file-name (buffer-file-name)))
-  "Directory where the scimax is installed.")
+;; taking ideas from https://github.com/jkitchin/scimax
+;;
+(defconst my-emacs-dir (file-name-directory (or load-file-name (buffer-file-name)))
+  "Directory where my-emacs is installed.")
 
-(defvar scimax-user-dir (expand-file-name "user" scimax-dir)
-  "User directory for personal code.")
+(defvar my-emacs-personal-dir (expand-file-name "personal" my-emacs-dir)
+  "Personal directory for personal code.")
 
-(setq user-emacs-directory scimax-user-dir)
+(setq user-emacs-directory my-emacs-dir)
 
-(setq package-user-dir (expand-file-name "elpa" scimax-dir))
+(setq package-user-dir (expand-file-name "elpa"  my-emacs-dir))
 
-
+;; From https://github.com/DarwinAwardWinner/dotemacs/blob/master/init.el
+;;
+;; Initialize the emacs packaging system
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives 
-'("melpa" . "https://melpa.org/packages/")) 
-(package-initialize) 
-;; Bootstrap `use-package' 
-(unless (package-installed-p
-'use-package) (package-refresh-contents) (package-install
-'use-package))
+(package-initialize)
 
-(require 'ido)
-(ido-mode t)
+;; Ensure latest org-mode is installed from
+;; http://orgmode.org/elpa.html
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-(visual-line-mode 1)
+(defun package-ensure (pkg &optional dont-select)
+  "Ensure PKG is installed, refreshing if necessary."
+  (unless (package-installed-p pkg)
+    (unless (memq pkg (mapcar #'car package-archive-contents))
+      (package-refresh-contents))
+    (package-install pkg dont-select)))
+
+(condition-case nil
+    (package-ensure 'org-plus-contrib)
+  (error (display-warning "Could not install latest org-mode. Falling back to bundled version.")))
+
+(condition-case nil
+    (package-ensure 'use-package)
+  (error (display-warning "Could not install use-package.")))
+
+(save-window-excursion 
+  (org-babel-load-file 
+   (expand-file-name "config.org" my-emacs-dir)))
