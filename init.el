9bcac1a4ad356a17c151e274f00a3adf44126025
Rafael Villarroel <rvf0068@gmail.com>
2020-10-05 21:34:26 -0500

    cl -> cl-lib for Emacs 27

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; see http://emacs.1067599.n8.nabble.com/use-package-tp396907p397337.html
 ;; we need it for the function 'case' in the 'by-backend' macro
-(require 'cl)
+(require 'cl-lib)
 
 ;; taking ideas from https://github.com/jkitchin/scimax
 ;;
